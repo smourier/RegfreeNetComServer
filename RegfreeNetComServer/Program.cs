5779ff9f34b4cefc3ccfbf527fb11fc599850cd7
Simon Mourier <simon_mourier@hotmail.com>
2024-01-24 15:09:28 +0100

    Initial version.

---
+++ RegfreeNetComServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace RegfreeNetComServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Windows Kits", "10"));
+        }
+    }
+}
